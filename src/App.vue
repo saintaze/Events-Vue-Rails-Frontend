630ecc749dacc7c1989124ac61f9a2ef60b241ed
ayaz ahmed <ayez_ahmed@hotmail.com>
2020-08-21 20:39:54 +0500

    creates toolbar component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <toolbar />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import Toolbar from '@/components/Toolbar.vue'
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Toolbar
+  }
 }
 </script>
 
 <style lang="scss">
+  html, body{
+    padding: 0;
+    margin: 0;
+  }
 </style>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    Event App
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'toolbar'
+}
+</script>
+
+<style lang="scss">
+  .header {
+    height: 40px;
+    line-height: 40px;
+    padding-left: 1.5em;
+    background: #eee;
+    border-bottom: 1px solid #ddd;
+    font-family: Lucida Grande,Helvetica,Arial,Verdana,sans-serif;
+    font-size: 14px;
+  }
+</style>
